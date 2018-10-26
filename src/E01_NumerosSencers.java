8f6fc20004dfd3b8970e9ebd01f813382689cac4
alexmoreitb <alexander.moreno.7e2@itb.cat>
2018-10-26 13:03:54 +0200

    P5

---
+++ src/E01_NumerosSencers.java
@@ -0,0 +1,17 @@
+/*
+Author: Alexander Alfonso Moreno Castro
+Starting date: 26/10/2018
+Ending date: 26/10/2018
+Objective: Program that requests 10 integers and show them by screen determining how many are positives, negatives or zero.
+ */
+import java.util.Scanner;
+public class E01_NumerosSencers {
+    //Declared variables
+    public static void main(String[] args){
+        Scanner requestValues = new Scanner(System.in);
+        int i;
+        int positive = 0;
+        int negative = 0;
+        int zero = 0;
+    }
+}
